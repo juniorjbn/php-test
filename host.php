ec9e3d78f06c9e60aeea78dbbcb14fa4a90b3af1
junior <juniorjbn@gmail.com>
2016-12-13 18:19:18 -0200

    Minha Mensagem de commit

--- host.php
+++ host.php
@@ -1,8 +1,8 @@
 <?php
 $hostname = getenv('HOSTNAME');
 if (substr($hostname, 0, 5) == 'siteb') {
-    echo 'Response from host:      ' . getenv('HOSTNAME') . "\n";
+    echo 'Response from container:      ' . getenv('HOSTNAME') . "\n";
 } else {
-    echo 'Response from host: ' . getenv('HOSTNAME') . "\n";
+    echo 'Response from container: ' . getenv('HOSTNAME') . "\n";
 }
 ?>
