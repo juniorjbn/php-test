05e09e45eb88dacbee3e9193e03d8e055b98c3d0
junior <juniorjbn@gmail.com>
2016-12-14 15:21:42 -0200

    update slack script

--- slack-hook.sh
+++ slack-hook.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+
+GIT_MSG=${git log -1 "--pretty=format:%ci %cn %h %s}
+
 curl -X POST --data-urlencode \
-payload="{'channel': '#codehip', 'username': 'JoãoBot', 'text': 'Build terminado, iniciando deploy da Aplicação "${OPENSHIFT_BUILD_NAME}" :package:.', 'icon_emoji': ':speaking_head_in_silhouette:'}" \
+payload="{'channel': '#codehip', 'username': 'GetupBOT', 'text': 'Novo Deploy Realizado da aplicação : "${OPENSHIFT_BUILD_NAME}" :octocat: Gerado a partir deste commit : "${GIT_MSG}" .', 'icon_emoji': ':speaking_head_in_silhouette:'}" \
 https://hooks.slack.com/services/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
+
