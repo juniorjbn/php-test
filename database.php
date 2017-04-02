b0888f88d36394ebe6e76633d767b3b8f5f70904
joao brito junior <juniorjbn@gmail.com>
2017-04-01 23:47:03 -0300

    Update database.php

--- database.php
+++ database.php
@@ -1,4 +1,11 @@
 <?php
+/* 
+Criar a tabela e inserir dados para pegar na query
+CREATE TABLE status (name VARCHAR(20), estado VARCHAR(20));
+INSERT INTO status (name,estado) VALUES("my","ok"); 
+
+*/
+
 $mysqli = new mysqli("galerinha:3306", "user", "vEYRj2BgpQhh", "userdb");
 
 /* check connection */
@@ -8,11 +15,19 @@ if (mysqli_connect_errno()) {
 }
 
 /* return name of current default database */
-if ($result = $mysqli->query("SHOW STATUS LIKE 'wsrep_cluster_size'")) {
+if ($result = $mysqli->query("SELECT estado FROM status")) {
     $row = $result->fetch_row();
     printf("Number of Cluster Replicas : %s\n", $row[1]);
     $result->close();
 }
+
+if ($result2 = $mysqli->query("SHOW STATUS LIKE 'wsrep_cluster_size'")) {
+    $row2 = $result2->fetch_row();
+    printf("Number of Cluster Replicas : %s\n", $row2[0]);
+    $result2->close();
+}
+
+
 
 $mysqli->close();
 ?>
