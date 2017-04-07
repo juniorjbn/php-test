985f421ba00f718b5ee803d136a0bdda8947c91c
diogogoebel <diogo.goebel@gmail.com>
2017-04-07 09:02:55 -0300

    ajuste H1

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <html>
 <head>
-    <title> Invisible infrastructure!</title> 
+    <title>Invisible infrastructure!</title> 
 </head>
 <body>
 
-<h1>Meu Teste PHP</h1>
+<h1>Apenas um Teste!</h1>
 
 <p>
     <a href=phpinfo.php>phpinfo</a>
