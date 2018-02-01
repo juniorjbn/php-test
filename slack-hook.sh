d66f6624c3b938bafa16bdbc42e1d81d2774adc6
joao <juniorjbn@gmail.com>
2018-02-01 18:56:33 -0200

    try slack again

--- slack-hook.sh
+++ slack-hook.sh
@@ -8,36 +8,7 @@
 
 #curl -v -XPOST --data-urlencode payload="{'channel': '#codehip', 'username': 'GetupBOT', 'text': 'Deploy "${OPENSHIFT_BUILD_NAME}" ("${OPENSHIFT_DEPLOYMENT_NAMESPACE}") :octocat: Commit: "${GIT_MSG}"', 'icon_emoji': ':speaking_head_in_silhouette:'}" $URL
 
-cat > /tmp/slack_text <<EOF
-{
-    "text": "Please review job ${BUILD_NUMBER}",
-    "channel:" "aristides"
-    "username:" "GetupBOT"
-    "attachments": [
-        {
-            "fallback": "You does not have permissions to approve",
-            "callback_id": "jenkins_approval",
-            "color": "#3AA3E3",
-            "attachment_type": "default",
-            "actions": [
-                {
-                    "name": "approval",
-                    "text": "Aprove",
-                    "type": "button",
-                    "value": "${BUILD_NUMBER}"
-                },
-                {
-                    "name": "approval",
-                    "text": "Decline",
-                    "type": "button",
-                    "value": "${BUILD_NUMBER}"
-                }
-            ]
-        }
-    ]
-}
-EOF
 
 curl -X POST -H 'Content-type: application/json' \
 https://hooks.slack.com/services/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 \
---data @/tmp/slack_text
+--data '{"text": "Please review job $BUILD_NUMBER","attachments": [{"fallback": "You does not have permissions to approve","callback_id": "jenkins_approval","color": "#3AA3E3","attachment_type": "default","actions": [{"name": "approval","text": "Aprove","type": "button","value": "$BUILD_NUMBER"},{"name": "approval","text": "Decline","type": "button","value": "$BUILD_NUMBER"}]}]}'
