b946a13edaeb56a84acb5b998e8458b8f2cfb9b6
joao <juniorjbn@gmail.com>
2018-02-01 18:03:13 -0200

    try new msg

--- slack-hook.sh
+++ slack-hook.sh
@@ -1,10 +1,43 @@
 #!/bin/bash
 
-GIT_LOG=$(git log -1 "--pretty=format:%cn %h %s")
+#GIT_LOG=$(git log -1 "--pretty=format:%cn %h %s")
 
-GIT_MSG=$(echo ${GIT_LOG// /_})
+#GIT_MSG=$(echo ${GIT_LOG// /_})
 
-URL=https://hooks.slack.com/services/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
+#URL=https://hooks.slack.com/services/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
 
-curl -v -XPOST --data-urlencode payload="{'channel': '#codehip', 'username': 'GetupBOT', 'text': 'Deploy "${OPENSHIFT_BUILD_NAME}" ("${OPENSHIFT_DEPLOYMENT_NAMESPACE}") :octocat: Commit: "${GIT_MSG}"', 'icon_emoji': ':speaking_head_in_silhouette:'}" $URL
+#curl -v -XPOST --data-urlencode payload="{'channel': '#codehip', 'username': 'GetupBOT', 'text': 'Deploy "${OPENSHIFT_BUILD_NAME}" ("${OPENSHIFT_DEPLOYMENT_NAMESPACE}") :octocat: Commit: "${GIT_MSG}"', 'icon_emoji': ':speaking_head_in_silhouette:'}" $URL
 
+cat > /tmp/slack_text <<EOF
+{
+    "text": "Please review job ${BUILD_NUMBER}",
+    "channel:" "aristides"
+    "username:" "GetupBOT"
+    "attachments": [
+        {
+            "fallback": "You does not have permissions to approve",
+            "callback_id": "jenkins_approval",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "approval",
+                    "text": "Aprove",
+                    "type": "button",
+                    "value": "${BUILD_NUMBER}"
+                },
+                {
+                    "name": "approval",
+                    "text": "Decline",
+                    "type": "button",
+                    "value": "${BUILD_NUMBER}"
+                }
+            ]
+        }
+    ]
+}
+EOF
+
+curl -X POST -H 'Content-type: application/json' \
+https://hooks.slack.com/services/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 \
+--data @/tmp/slack_text
