2f41c896d8b415561b3ba62fbb904235c6c18db2
joao <juniorjbn@gmail.com>
2018-02-02 10:33:55 -0200

    .

--- slack-hook.sh
+++ slack-hook.sh
@@ -11,4 +11,4 @@
 
 curl -X POST -H 'Content-type: application/json' \
 https://hooks.slack.com/services/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 \
---data '{"text": "Please review job ${BUILD_NUMBER}","attachments": [{"fallback": "You does not have permissions to approve","callback_id": "jenkins_approval","color": "#3AA3E3","attachment_type": "default","actions": [{"name": "approval","text": "Aprove","type": "button","value": "${BUILD_NUMBER}"},{"name": "approval","text": "Decline","type": "button","value": "${BUILD_NUMBER}"}]}]}'
+--data "{\"text\": \"Please review job ${BUILD_NUMBER}\",\"attachments\": [{\"fallback\": \"You does not have permissions to approve\",\"callback_id\": \"jenkins_approval\",\"color\": \"#3AA3E3\",\"attachment_type\": \"default\",\"actions\": [{\"name\": \"approval\",\"text\": \"Aprove\",\"type\": \"button\",\"value\": \"${BUILD_NUMBER}\"},{\"name\": \"approval\",\"text\": \"Decline\",\"type\": \"button\",\"value\": \"${BUILD_NUMBER}\"}]}]}"
