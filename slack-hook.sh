4412dc81fc649a9956a1a3aa1c938da8f76b3bb9
joao brito junior <juniorjbn@gmail.com>
2019-07-10 13:01:00 -0300

    Update slack-hook.sh

--- slack-hook.sh
+++ slack-hook.sh
@@ -4,7 +4,6 @@
 
 #GIT_MSG=$(echo ${GIT_LOG// /_})
 
-#URL=https://hooks.slack.com/services/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
 
 #curl -v -XPOST --data-urlencode payload="{'channel': '#codehip', 'username': 'GetupBOT', 'text': 'Deploy "${OPENSHIFT_BUILD_NAME}" ("${OPENSHIFT_DEPLOYMENT_NAMESPACE}") :octocat: Commit: "${GIT_MSG}"', 'icon_emoji': ':speaking_head_in_silhouette:'}" $URL
 
